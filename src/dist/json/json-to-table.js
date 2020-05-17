1e6e96c3b68d6d0c71ca66e43ec62492f7bc494d
sixing <sixing@qunhemail.com>
2020-05-17 22:38:42 +0800

    新增随机显示单词数

--- src/dist/json/json-to-table.js
+++ src/dist/json/json-to-table.js
@@ -13,8 +13,17 @@ var json2table = function (json, classes) {
     cols.map(function(col) {
         headerRow += '<th>' + capitalizeFirstLetter(col) + '</th>';
     });
+    var showTdIds = null;
+    if(randomNum != null){
+        var arr = randomArr(json.length);
+        showTdIds = randomNumBoth(arr,randomNum > json.length ? json.length : randomNum );
+    }
 
-    json.map(function(row) {
+    json.map(function(row,i) {
+        if(!isShow(i,showTdIds)){
+           return;
+        }
+
         bodyRows += '<tr>';
 
         cols.map(function(colName,index) {
@@ -53,9 +62,18 @@ var json2table = function (json, classes) {
 }
 
 var isAlpha = false;
+var randomNum = null;
 
-var json2tableByAlpha = function (json) {
+var isShow = function (i,showTdIds,) {
+    if(showTdIds == null){
+        return true;
+    }
+    return showTdIds.indexOf(i) >-1;
+}
+
+var json2tableByAlpha = function (json,random) {
     isAlpha = true;
+    randomNum = random;
     return json2table(json,'');
 }
 
@@ -82,3 +100,34 @@ var alphaPlay =function (e,isFemale) {
     sound.play();
 }
 
+
+//循环创建一个数组的函数
+function randomArr(maxcount){
+    var arr = [];
+    for(var i=0;i<maxcount;i++){
+        arr.push(i)
+    }
+    return arr;
+}
+
+
+//取出随机数, maxNum为 取出随机数的个数
+function randomNumBoth(arr,maxNum){
+    var numArr = [];
+    //numArr.push(0);
+    //最大的循环次数
+    var arrLength = arr.length;
+    for(var i = 0;i<arrLength;i++){
+        //获取arr的长度
+        var Rand = arr.length
+        //取出随机数
+        var number = Math.floor(Math.random()*arr.length); //生成随机数num
+        //往新建的数组里面传入数值
+        numArr.push(arr[number]);
+        //传入一个删除一个，避免重复
+        arr.splice(number,1);
+        if(arr.length <= arrLength-maxNum){
+            return numArr;
+        }
+    }
+}
--- wiki/home.md
+++ wiki/home.md
@@ -2,6 +2,7 @@
 - 单词词频统计的dubbo、flink、jdk8、rocketmq、spring相关框架的源码，有助于大家快速掌握源码。
 - 单词读音来源[滑词](https://translate.google.cn)和[双击](http://dict.cn)。
 - [词库下载](https://pan.baidu.com/s/1cebgk2cfl-kDy8Zf6TVvng)，解压后放置在本地/storage/alpha目录
+- - 随机最多显示[10]({{baseDomain}}/?random=10)、[20]({{baseDomain}}/?random=20)、[50]({{baseDomain}}/?random=50)、[100]({{baseDomain}}/?random=100)、[200]({{baseDomain}}/?random=200)、[500]({{baseDomain}}/?random=500)、[1000]({{baseDomain}}/?random=1000)个单词
 
 ### 名词  
 ```json2tableByAlpha
