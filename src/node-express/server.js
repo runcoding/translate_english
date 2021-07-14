040e1f278ff06a783b750310e4cf63909353979f
RunningGhost <runcoding@163.com>
2021-07-14 17:12:37 +0800

    Update server.js

--- src/node-express/server.js
+++ src/node-express/server.js
@@ -1,14 +1,10 @@
 'use strict';
-
 var express = require('express');
 var path = require('path');
 var PORT = 3666;
-
 var app = express();
-
 app.use(express.static(path.join(__dirname, '../../')));
 /**加载本地词库地址*/
 app.use(express.static('/storage'));
-
 app.listen(PORT);
-console.log('Running  on http://localhost:' + PORT);
+console.log('欢迎访问 http://localhost:' + PORT);
